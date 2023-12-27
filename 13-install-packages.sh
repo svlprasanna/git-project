d3da009a270eaf5522050ba43ecebc139c90dbc4
Lakshmi Mohan <svlprasanna92@gmail.com>
2023-12-27 21:36:48 +0530

    username input prompt

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -3,7 +3,19 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y='\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 failed to install the package"
+}
 
 if [ $ID -ne 0 ]
 then
@@ -12,4 +24,13 @@ then
 else
     echo -e "$N you are $R root user"
 fi
+echo "all arguments are $@"
 
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+    yum install $package -y
+    VALIDATE $? "Installation of $package"
+done
