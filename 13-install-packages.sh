b306dd6e50e44fa083ac6a633808631b12c0c77b
Lakshmi Mohan <svlprasanna92@gmail.com>
2023-12-27 20:27:35 +0530

    username input prompt

---
+++ 13-install-packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+if [ $ID -ne 0]
+then
+    echo -e "$R ERROR: Please run this script with root access $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
