0acb3d549d9f9886f7fd8479c792c5ecc2e7d261
Lakshmi Mohan <svlprasanna92@gmail.com>
2023-12-27 18:48:08 +0530

    username input prompt

---
+++ 11.logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name: $0"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+then
+    echo "ERROR:......$2 failed"
+    exit 1 
+else
+    echo "....$2..success"
+fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:PLEASE RUN THE SCRIPT WITH ROOT ACCESS"
+    exit 1
+else
+    echo "you are root user"
+fi
+yum install mysql -y
+VALIDATE $? "Installing mysql"
+yum install git -y
+VALIDATE $? "Installing git"
