1cbf8a5f38573facf84136f36ecfb051a401d42c
Lakshmi Mohan <svlprasanna92@gmail.com>
2023-12-27 16:24:00 +0530

    username input prompt

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,10 +4,18 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then
-echo "ERROR:PLEASE RUN THE SCRIPT WITH ROOT ACCESS"
-exit 1
+    echo "ERROR:PLEASE RUN THE SCRIPT WITH ROOT ACCESS"
+    exit 1
 else
-echo "you are root user"
+    echo "you are root user"
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "installing mysql is failed"
+    exit 1
+else
+    echo "installing mysql is success"
+fi
