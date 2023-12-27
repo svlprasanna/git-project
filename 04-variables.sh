87e4e3e12bd4be110e3d54da7a954eca91b9964f
Lakshmi Mohan <svlprasanna92@gmail.com>
2023-12-27 14:31:46 +0530

    something new added

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1 is doctor"
+echo "$person2 is engineer"
+echo "$person1 want to become lawyer"
+echo "$person2 want to wish wish you all happy morning"
+
+DATE=$(date)
+echo "date and Time is ${DATE}"
